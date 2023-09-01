37dc7825163ab7391d2af721e02e780825ab47a7
ArtyomZ <dcclxxviiz@gmail.com>
2023-09-01 07:53:33 -0400

    lesson2

---
+++ Lesson_2/Mounth.rb
@@ -0,0 +1,7 @@
+months = {'january' => 31, 'february' => 28, 'march' => 31, 'april' => 30, 'may' => 31, 'june' => 30, 'july' => 31, 'august' => 31, 'september' => 30, 'october' => 31, 'november' => 30, 'december' => 31}
+
+for i in (0...11)
+  if months.values[i] == 30
+    puts "In #{months.keys[i]} 30 days"
+  end
+end
